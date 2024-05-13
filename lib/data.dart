1c54fba07fc0c6916780e465a9d72c5013f99401
tauqeerkhattak <tauqeer745@outlook.com>
2024-05-13 16:11:42 +0500

    update - infinite scroll feature added to card animation

---
+++ lib/data.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Data {
+  final int index;
+  final double top;
+  final double left;
+  final int angleInDegrees;
+  final MaterialColor color;
+
+  Data({
+    required this.index,
+    required this.top,
+    required this.left,
+    required this.angleInDegrees,
+    required this.color,
+  });
+
+  Data copyWith({
+    int? index,
+    double? top,
+    double? left,
+    int? angleInDegrees,
+  }) {
+    return Data(
+      index: index ?? this.index,
+      top: top ?? this.top,
+      left: left ?? this.left,
+      angleInDegrees: angleInDegrees ?? this.angleInDegrees,
+      color: color,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'INDEX: $index, TOP: $top, LEFT: $left, ANGLE: $angleInDegrees';
+  }
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+
+import '../../../data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +11,10 @@ class HomePage extends StatefulWidget {
   State createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   List<MaterialColor> cardColors = [
-    Colors.purple,
+    Colors.pink,
     Colors.amber,
     Colors.cyan,
     Colors.orange,
@@ -20,46 +23,134 @@ class _HomePageState extends State<HomePage> {
     Colors.red,
     Colors.blue,
   ];
+  List<Data> dataList = [];
+  bool isAnimating = false;
+  late Offset outOfBoundsOffset;
+  late Data savedData;
+  bool isFirstTime = true;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final size = MediaQuery.sizeOf(context);
+      outOfBoundsOffset = Offset(size.width * 2, size.height * 2);
+
+      for (int i = 0; i < cardColors.length; i++) {
+        final top = -(50 * i).toDouble();
+        final left = (15 * i).toDouble();
+        final angleInDegrees = (-25 + (i * 3));
+        final data = Data(
+          index: i,
+          top: top,
+          left: left,
+          angleInDegrees: angleInDegrees,
+          color: cardColors[i],
+        );
+        dataList.add(data);
+      }
+      dataList.add(
+        Data(
+          index: 7,
+          top: outOfBoundsOffset.dy,
+          left: outOfBoundsOffset.dx,
+          angleInDegrees: (-25 + (7 * 3)),
+          color: cardColors[7],
+        ),
+      );
+    });
+  }
+
+  Future<void> _handleDownGesture() async {
+    savedData = dataList.last;
+    for (int i = dataList.length - 1; i >= 0; i--) {
+      Data current = dataList[i];
+      Data newCurrent = current.copyWith(
+        top: savedData.top,
+        left: savedData.left,
+        angleInDegrees: savedData.angleInDegrees,
+      );
+      savedData = current;
+      dataList[i] = newCurrent;
+    }
+
+    final first = dataList.first;
+    dataList[dataList.length - 1] = dataList.last.copyWith(
+      top: first.top,
+      left: first.left,
+      angleInDegrees: first.angleInDegrees,
+    );
+    isFirstTime = false;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildBody(),
+    return GestureDetector(
+      onPanUpdate: (details) {
+        if (isAnimating) {
+          return;
+        }
+        isAnimating = true;
+        if (details.delta.dy > 0) {
+          _handleDownGesture();
+        } else if (details.delta.dy < 0) {
+          print('UP');
+        }
+      },
+      onPanEnd: (details) {
+        isAnimating = false;
+        print('DONE ANIMATING');
+      },
+      child: Scaffold(
+        body: _buildBody(),
+      ),
     );
   }
 
   Widget _buildBody() {
+    dataList.sort((date1, date2) {
+      return date2.index.compareTo(date1.index);
+    });
     return Stack(
       children: [
-        for (int i = cardColors.length - 1; i >= 0; i--) _buildContainer(i),
+        for (int i = 0; i < dataList.length; i++)
+          _buildCard(
+            dataList[i],
+          ),
       ],
     );
   }
 
-  Widget _buildContainer(int index) {
+  Widget _buildCard(Data data) {
     final width = MediaQuery.sizeOf(context).width;
-    final height = MediaQuery.sizeOf(context).height;
-    return Positioned(
-      top: -(50 * index).toDouble(),
-      left: (15 * index).toDouble(),
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 500),
+      top: data.top,
+      left: data.left,
       width: width * 1.6,
       child: Transform.translate(
-        offset: Offset(width * 0.3, height * 0.2),
-        child: RotationTransition(
-          turns: AlwaysStoppedAnimation((-25 + (index * 3)) / 360),
+        offset: Offset(width * 0.62, 0),
+        child: AnimatedRotation(
+          duration: const Duration(milliseconds: 500),
+          alignment: Alignment.bottomRight,
+          turns: data.angleInDegrees.toDouble() / 360,
           child: Container(
-            height: 700,
+            height: 800,
+            width: width * 1.6,
             decoration: BoxDecoration(
+              boxShadow: kElevationToShadow[1],
               gradient: LinearGradient(
                 begin: Alignment.bottomCenter,
                 end: Alignment.topCenter,
                 colors: [
-                  cardColors[index].shade900,
-                  cardColors[index],
-                  cardColors[index].shade100,
+                  data.color.shade900,
+                  data.color,
+                  data.color.shade100,
                 ],
               ),
             ),
+            child: Text('${data.index}'),
           ),
         ),
       ),
@@ -67,6 +158,10 @@ class _HomePageState extends State<HomePage> {
   }
 
   double toRadians(int degrees) {
-    return degrees * (pi / 180);
+    return degrees * (math.pi / 180);
+  }
+
+  double toDegrees(double radians) {
+    return radians * (180 / math.pi);
   }
 }
